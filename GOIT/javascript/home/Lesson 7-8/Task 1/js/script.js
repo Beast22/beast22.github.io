ecc8f89e72add9db452b0dd4180378369fc95317
Eugene Snakecatcher <blagorad@list.ru>
2015-12-24 21:04:12 +0300

    jq to Denis

--- GOIT/javascript/home/Lesson 7-8/Task 1/js/script.js
+++ GOIT/javascript/home/Lesson 7-8/Task 1/js/script.js
@@ -1,8 +1,15 @@
+		$('.menu_item:first').addClass ('active');
+    	$('#cont_tab1').addClass ('cont_active');
 
+    $('.menu_item').on('click', function(){
+    	$('.menu_item').removeClass('active');
+    	$(this).addClass('active');
+    	var id = $(this).data('toggle');
+ 		$('#' + id).addClass('cont_active');
+    	$('#' + $(this).data('toggle')).addClass('cont_active');
 
-    $(function(){
-    	$('.menu_item:first').addClass ('active');
-    	$('#cont_tab:first').addClass ('cont_active');
-    	
+  //   	var num_active = $('.menu_item').find('.active').index();
+  //   	$('#cont_tab').removeClass('cont_active');
+		// $('#cont_tab').eq(num_active).addClass('cont_active');
 	});
 
