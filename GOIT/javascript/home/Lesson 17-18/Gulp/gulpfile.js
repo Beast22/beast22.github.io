7a576e36a1db7f7b71dc76abb6278599fb1059af
Eugene Snakecatcher <blagorad@list.ru>
2016-02-10 03:41:33 +0300

    task-runners finished

--- GOIT/javascript/home/Lesson 17-18/Gulp/gulpfile.js
+++ GOIT/javascript/home/Lesson 17-18/Gulp/gulpfile.js
@@ -1,20 +1,47 @@
-'use strict';
 
 
-	var gulp = require('gulp'),
+	var gulp = require('gulp');
 	concatCss = require('gulp-concat-css');
 	minifyCss = require('gulp-minify-css');
+	rename = require("gulp-rename");
+	notify = require("gulp-notify");
+	prefix = require("gulp-autoprefixer");
+	concat = require('gulp-concat');
+	uglify = require('gulp-uglify');
+	imagemin = require('gulp-imagemin');
 
-gulp.task('concat-css', function() {
+gulp.task('styles', function() {
     return gulp.src(['css/carousel.css','css/style.css'])
-    .pipe(concatCss("total.css"))
-    .pipe(gulp.dest('build/css/'));
+	    .pipe(concatCss("total.css"))
+	    .pipe(prefix({
+	    	browsers: ['last 2 versions', '> 1%', 'ie 9'],
+			cascade: false
+	    }))
+	    .pipe(minifyCss())
+	    .pipe(rename("total.min.css"))
+	    .pipe(gulp.dest('build/css/'))
+	    .pipe(notify('Succesfully!'));
+
 });
 
-gulp.task('minify-css', function() {
-    return gulp.src('build/css/total.css')
-    .pipe(concatCss("total_min.css"))
-    .pipe(gulp.dest('build/css/'));
+gulp.task('watch', function() {
+    return gulp.watch('css/*css', ['styles']);
 });
 
-gulp.task('default', ['concatCss', 'minifyCss']);
+gulp.task('scripts', function() {
+	 return gulp.src(['js/menu.js','js/carousel.js'])
+	    .pipe(concat('script.js'))
+	    .pipe(uglify())
+	    .pipe(rename('myscript.js'))
+	    .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('img', function() {
+	return gulp.src('img/*')
+		.pipe(imagemin({
+		progressive: true
+	}))
+	.pipe(gulp.dest('build/img'));
+});
+
+gulp.task('default', ['styles', 'scripts', 'img']);
