4cf298a688a35f77c1e31bb7f97505fb2a2f1cf2
Eugene Snakecatcher <blagorad@list.ru>
2016-02-27 21:51:23 +0300

    jasmin

--- GOIT/javascript/home/Lesson 21-22/dist/js/script.js
+++ GOIT/javascript/home/Lesson 21-22/dist/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-$(function () {
+window.app = $(function () {
 
 	// let Template();
 
---
+++ GOIT/javascript/home/Lesson 21-22/spec/spec.js
@@ -0,0 +1,5 @@
+describe("test service info in template arguments", function(){
+	it("test`s title", function(){
+		expect(app).toBeDefined();
+	});
+});
