98d9b9139ea1fa105b1705ec8b7bbf14874231e3
Eugene Snakecatcher <blagorad@list.ru>
2016-04-05 16:27:23 +0400

    exam next

--- GOIT/javascript/home/exam/src/js/script.js
+++ GOIT/javascript/home/exam/src/js/script.js
@@ -1,4 +1,4 @@
-// sliders
+/*------sliders------*/
 
 $(window).load(function() {
   $('.flexslider').flexslider({
@@ -10,123 +10,50 @@ $(window).load(function() {
   });
 });
 
-(function($){
+/*------ajax------*/
 
-	var pict = [],
-		defaultImg = [
-		{ url: "dist/img/gall-1.jpg", word: "Sport and Activity" },
-		{ url: "dist/img/gall-2.jpg", word: "Wellnes and Health" },
-		{ url: "dist/img/gall-3.jpg", word: "Extreme Sports and Expeditions"},
-		{ url: "dist/img/gall-4.jpg", word: "Games"},
-		{ url: "dist/img/gall-5.jpg", word: "Culture and Education"},
-		{ url: "dist/img/gall-6.jpg", word: "Relaxation"},
-		{ url: "dist/img/gall-7.jpg", word: "Travelling"}
-		];
+$(function() {
 
-	function getQuery(){
-		var userQuery = $('.activity-form__input').val();
-		$('.activity-form__input').val("");
-		initQuery(userQuery);
-	}
-
-	function initQuery(userQuery){
-		var query = encodeURIComponent(userQuery);
-		var queryStr = 'http://api.pixplorer.co.uk/image?word=' + query + '&amount=7&size=s';
-
+	function renderImg(queryImg){
 		$.ajax({
 			type: "GET",
-			dataType: "jsonp",
-			cache: false,
-			url: queryStr,
-			success: function(data) {
-				if ( data.status !== 'failed' ) {
-					if (data.pict.length > 0) {
-					localStorage.setItem("word", userQuery);
-					var i = 0;
-					while(i < 7) {
-						pict[i].url = data.pict[i].imageurl;
-						pict[i].word = data.pict[i].word;
-						i++;
-					}
-				} else {
-						if (localStorage.word) {
-							$.each(pict, function(i, img) {
-								img.word = localStorage.word;
-							});
-							}
-				}
-				} else{
-						pict = copy(defaultImg);
+	        dataType: "json",
+	        cache: false,
+	        url: 'http://api.pixplorer.co.uk/image?word=' + queryImg + '&amount=7&size=m',
 
-				}
-				render(pict);	
-			},
+	        success: function(data){
+	        	for (i = 0; i <= (data.images.length - 1); i++){
+	        		url1 = data.images[i].imageurl;
+	        		$('.grid-item').eq(i).css("background-image", "url('"+decodeURI(url1)+"')");
+	        		$('.activity__text').eq(i).text(data.images[i].word);
+	        	}
 
-			error: function() {
-				alert('Ajax does not support by this browser.');
-			}
-		}).done(function(){
-			Isotope();
-		});
-	}
+	             $('.grid').masonry({
+	                itemSelector: '.grid-item', 
+	                 columnWidth: '.gutter-sizer', 
+	                 isResizable: true, 
+	                 singleMode: false, 
+	                 isAnimated: true, 
+	                 animationOptions: { 
+	                      queue: false,
+	                      duration: 400
+	                 }
+	              });
+	        }
 
+	     });
+	}   
 
-	function Isotope(){
-		$('.images-dest').isotope({
-			layoutMode: 'masonry',
-			itemSelector: '.activity__link',
-			masonry: {
-				gutter: 20
-			}
-		});
+	        $('.block__button--activity').click(function(e) {
 
-	}
+		        e.preventDefault();
+		        var query = encodeURIComponent($('.activity-form__input').val());
+		        renderImg(query);
+		        return false;
 
-	function render(pict){
-		var html = tmpl($('#_template').html(), { data:pict });
-		$('.images-dest').html(html);
-	}
+	    	});
 
-	function copy(oldArray) {
-		var newArray = [];
-		$.each(oldArray, function(i){
-			newArray[i] = $.extend({}, oldArray[i]);
-		});
-
-		return newArray;
-	}
-
-	$(function() {
-		pict = copy(defaultImg);
-
-
-		if(typeof(Storage) !== "undefined") {
-
-			var loadedData = localStorage.getItem("word");
-
-			if (loadedData) {
-				initQuery(loadedData);
-			} else {
-				initQuery('');
-			}
-
-	} else {
-				var renderTmpl = function() {
-					return render(pict);
-				}
-
-				$.when(renderTmpl()).done(function() {
-					Isotope();
-				});
-		}
-	});
-
-	$('#form-activity').submit(function(e){
-		e.preventDefault();
-		$('.images-dest').isotope('destroy');
-		$('.images-dest').html("");
-		getQuery();
-	});	
-
-})(jQuery);
+	    	renderImg('');
+	
+})
 
